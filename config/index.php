53e5bf64d0cb6d8ac5decf343df1fb57f2163eb8
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2019-09-25 22:41:47 +0200

    modification bug menu

---
+++ config/index.php
@@ -0,0 +1,23 @@
+<!-- Auteur Aymeric Bizouarn -->
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device.width, initial-scale=1.0">
+    <meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+    <title>Planning</title>
+    <link id="stylesheet" rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="../image/iut-vannes2.png"/>
+    <script src="script.js"></script>
+</head>
+<body>
+<?php
+if(isset($_SESSION['id'])){
+
+} else {
+
+}
+?>
+?>
+</body>
