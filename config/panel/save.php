21f81b27b1913b7d606c434ea4ca2b841cf9ad2a
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2019-09-28 19:28:41 +0200

    Correction faille.

--- config/panel/save.php
+++ config/panel/save.php
@@ -1,13 +1,15 @@
 <form>
     <?php
-    if (isset($_POST['saveText']) && isset($_POST['file'])) {
-        echo "<h1>Information Enregistrer</h1>";
-        file_put_contents("data/" . $_POST['file'], $_POST['saveText']);
-    } else {
-        echo "<h1>Ereur</h1>";
-    }
-    echo "<p class=\"submit\">
-        <input onclick=\"redirection(\" ?page=".$_POST['page']."\")\" type=\"submit\" name=\"commit\" value=\"retour au menu\">
+    if(isset($_SESSION['id'])) {
+        if (isset($_POST['saveText']) && isset($_POST['file'])) {
+            echo "<h1>Information Enregistrer</h1>";
+            file_put_contents("data/" . $_POST['file'], $_POST['saveText']);
+        } else {
+            echo "<h1>Ereur</h1>";
+        }
+        echo "<p class=\"submit\">
+        <input onclick=\"redirection(\" ?page=" . $_POST['page'] . "\")\" type=\"submit\" name=\"commit\" value=\"retour au menu\">
     </p>";
+    }
     ?>
 </form>
