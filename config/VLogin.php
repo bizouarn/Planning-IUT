b0ed100acd334a5419d856e00ca9386078025571
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2020-03-07 10:06:44 +0100

    Changement de mots de passe config

--- config/VLogin.php
+++ config/VLogin.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (isset($_POST['login']) && isset($_POST['password'])) {
-    if (strcmp($_POST['login'], 'admin') == 0 && strcmp($_POST['password'], '#adminBDE') == 0) {
-        $_SESSION['id'] = $_POST['login'];
-        echo("connection réussit : " . $_SESSION['id']);
+if(isset($_POST['login']) && isset($_POST['password'])){
+    if(strcmp($_POST['login'],'adminBDE')==0 && strcmp($_POST['password'],'#BDEPassAdmin')==0){
+        $_SESSION['id']=$_POST['login'];
+        echo("connection réussit : ".$_SESSION['id']);
     }
 }
 header('Location: index.php');
